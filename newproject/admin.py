75b555c00add6f09d7413d27718b4726ba57ab7d
공경섭 <kyong.dev@gmail.com>
2024-07-29 20:33:30 +0900

    unfold setup

---
+++ newproject/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = '관리자 페이지'
+admin.site.site_title = '관리자 페이지'
+admin.site.index_title = '관리자 페이지'
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
+from unfold.admin import ModelAdmin
+from django.contrib.auth.models import Group
 
-admin.site.register(AppUser)
-admin.site.register(SocialAccount)
-admin.site.register(Admin, UserAdmin)
+
+@admin.register(Admin)
+class AdminUserAdmin(ModelAdmin):
+    pass
+
+@admin.register(AppUser)
+class AppUserAdmin(ModelAdmin):
+    pass
+
+@admin.register(SocialAccount)
+class SocialAccountAdmin(ModelAdmin):
+    pass
+
+admin.site.unregister(Group)
+@admin.register(Group)
+class GroupAdmin(ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
