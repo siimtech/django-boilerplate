2173d662c66828354dac1148f5567dda1bd998c6
공경섭 <kyong.dev@gmail.com>
2024-07-30 19:12:07 +0900

    admin dashboard dummy

--- newproject/views.py
+++ newproject/views.py
@@ -1,68 +1,119 @@
 from django.shortcuts import render
 from typing import Dict
 from django.http import HttpRequest
+from users.models import AppUser
+from utils.timezone_utils import get_start_of_today
+import json
 
 def index(request):
     return render(request, 'index.html')
 
 def dashboard_callback(request: HttpRequest, context) -> Dict:
-
+    def get_total_user():
+        return AppUser.objects.count()
+    
     bar_chart_data = {
-        "labels": [
-            "07/22",
-            "07/23",
-            "07/24",
-            "07/25",
-            "07/26",
-            "07/27",
-            "07/28"
-        ],
-        "datasets": [
-            {
-                "label": "매출",
-                "backgroundColor": "rgba(255, 99, 132, 0.5)",
-                "data": [
-                    20372365,
-                    21077579,
-                    21577157,
-                    25058620,
-                    29522239,
-                    2496161,
-                    24914666
-                ],
-                "stack": "Stack 0"
-            },
-            {
-                "label": "매입",
-                "backgroundColor": "rgba(0, 156, 123, 0.5)",
-                "data": [
-                    13751566,
-                    16303560,
-                    31039019,
-                    33621116,
-                    34660119,
-                    0,
-                    0
-                ],
-                "stack": "Stack 1"
+            "data": json.dumps({
+            "labels": [
+                "07/22",
+                "07/23",
+                "07/24",
+                "07/25",
+                "07/26",
+                "07/27",
+                "07/28"
+            ],
+            "datasets": [
+                {
+                    "label": "매출",
+                    "backgroundColor": "rgba(196, 209, 197, 1)",
+                    "data": [
+                        20372365,
+                        21077579,
+                        21577157,
+                        25058620,
+                        29522239,
+                        2496161,
+                        24914666
+                    ],
+                    "stack": "Stack 0"
+                },
+                {
+                    "label": "매입",
+                    "backgroundColor": "rgba(201, 157, 202, 1)",
+                    "data": [
+                        13751566,
+                        16303560,
+                        31039019,
+                        33621116,
+                        34660119,
+                        0,
+                        0
+                    ],
+                    "stack": "Stack 1"
+                }
+            ]
+        }),
+        "options": json.dumps({
+            "scales": {
+                "x": {
+                    "stacked": True
+                },
+                "y": {
+                    "stacked": True
+                }
             }
-        ]
+        })
     }
 
-    bar_chart_data = "{\"labels\":[\"07/22\",\"07/23\",\"07/24\",\"07/25\",\"07/26\",\"07/27\",\"07/28\"],\"datasets\":[{\"label\":\"매출\",\"backgroundColor\":\"rgba(255,99,132,0.5)\",\"data\":[20372365,21077579,21577157,25058620,29522239,2496161,24914666],\"stack\":\"Stack0\"},{\"label\":\"매입\",\"backgroundColor\":\"rgba(0,156,123,0.5)\",\"data\":[13751566,16303560,31039019,33621116,34660119,0,0],\"stack\":\"Stack1\"}]}"
+    
+    line_chart_data = {
+            "data": json.dumps({
+            "labels": [
+                "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", 
+                "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", 
+                "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+            ],
+            "datasets": [
+                {
+                    "data": [
+                        [1, 13], [1, 21], [1, 12], [1, 23], [1, 26], [1, 15], [1, 16], 
+                        [1, 8], [1, 17], [1, 10], [1, 18], [1, 20], [1, 10], [1, 20], 
+                        [1, 9], [1, 21], [1, 11], [1, 14], [1, 8], [1, 15], [1, 16], 
+                        [1, 18], [1, 14], [1, 10], [1, 9], [1, 20], [1, 10]
+                    ],
+                    "borderColor": "#9333ea"
+                }
+            ]
+        }),
+        "options": {}
+    }
+    
     context.update({
         "cards": [
             {
-                "title": "Card 1",
-                "footer": "Footer 1",
-                "metric": "Label 1",
-                "icon": "icon 1",
+                "title": "총 유저",
+                "metric": get_total_user(),
+                "icon": "people",
+                # "footer": "Footer 1",
             },
             {
-                "title": "Card 2",
-                "footer": "Footer 2",
-                "metric": "Label 2",
-                "icon": "icon 2",
+                "title": "신규가입",
+                "metric": "1",
+                "icon": "person_add",
+                # "footer": "Footer 2",
+            },
+            {
+                "title": "인증대기 유저",
+                "metric": "1",
+                "icon": "hourglass",
+                # "footer": "Footer 3",
+            },
+            {
+                "title": "MAU",
+                "metric": "0",
+                "icon": "monitoring",
+                # "footer": "Footer 3",
             },
         ],
         "table_data": {
@@ -73,5 +124,61 @@ def dashboard_callback(request: HttpRequest, context) -> Dict:
             ]
         },
         "bar_chart_data": bar_chart_data,
+        "line_chart_data": line_chart_data,
+        "table_data": {
+            "headers": ["유저네임", "이름", "활동", "가입일"],
+            "rows": [
+                ["user1", "홍길동", "2024-07-30", "2023-01-01"],
+                ["user2", "김철수", "2024-07-30", "2023-02-15"],
+                ["user3", "이영희", "2024-07-30", "2023-03-20"],
+                ["user4", "박민수", "2024-07-29", "2023-04-10"],
+                ["user5", "최지우", "2024-07-29", "2023-05-05"]
+            ]
+        },
+        "progress_data": [
+            {
+                "title": "Daily Exercise",
+                "description": "Completed daily exercise routine",
+                "value": 80
+            },
+            {
+                "title": "Reading",
+                "description": "Read 30 pages of a book",
+                "value": 60
+            },
+            {
+                "title": "Meditation",
+                "description": "Meditated for 20 minutes",
+                "value": 90
+            },
+            {
+                "title": "Coding Practice",
+                "description": "Completed coding challenges",
+                "value": 70
+            },
+            {
+                "title": "Water Intake",
+                "description": "Drank 8 glasses of water",
+                "value": 100
+            },
+            {
+                "title": "Sleep",
+                "description": "Slept for 8 hours",
+                "value": 85
+            }
+        ]
     })
     return context
+
+
+def user_badge_callback(request):
+    start_of_today = get_start_of_today()
+    count = AppUser.objects.filter(registered_at__gte=start_of_today).count()
+    if count > 0:
+        return count
+    else:
+        return ''
+    
+def admin_permission_callback(request):
+    return request.user.is_superuser
+    # return request.user.has_perm("newproject.change_model")
---
+++ users/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from django.core.validators import RegexValidator
+from .models import AppUser, Admin
+from unfold.widgets import UnfoldAdminTextInputWidget, UnfoldAdminImageFieldWidget, UnfoldBooleanSwitchWidget
+    
+class AppUserForm(forms.ModelForm):
+    username = forms.CharField(
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+$',
+                message='50자 이하 문자, 숫자 그리고 @/./+/-/_만 가능합니다.',
+            ),
+        ], 
+        widget=UnfoldAdminTextInputWidget(attrs={'placeholder': '50자 이하 문자, 숫자 그리고 @/./+/-/_만 가능합니다.'}),
+        help_text='50자 이하 문자, 숫자 그리고 @/./+/-/_만 가능합니다.',
+        label='유저네임'
+    )
+    phone_number = forms.CharField(
+        required=False, 
+        widget=UnfoldAdminTextInputWidget(attrs={'placeholder': '010-1234-5678'}),
+        validators=[
+            RegexValidator(
+                regex=r'^010-\d{3,4}-\d{4}$',
+                message='휴대폰 번호 형식이 맞지 않습니다.'
+            )
+        ],
+        label='휴대폰 번호'
+    )
+    is_phone_verified = forms.BooleanField(
+        required=False, 
+        disabled=True,
+        widget=UnfoldBooleanSwitchWidget(),
+        help_text='휴대폰 인증여부는 관리자페이지에서 수정하실 수 없습니다.',
+        label='휴대폰 인증 여부'
+    )
+
+    class Meta:
+        model = AppUser
+        fields = '__all__'
+        help_texts = {
+            'is_active': '이 사용자가 활성화되어 있는지를 나타냅니다. 계정을 삭제하는 대신 이것을 선택 해제하세요.',
+        }
+        readonly_fields = ('is_phone_verified',)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password')
+        if user.id is None or not user.check_password(password):
+            user.set_password(password)
+            if commit:
+                user.save()
+        return user
+    
+class AdminUserForm(forms.ModelForm):
+    class Meta:
+        model = Admin
+        fields = '__all__'
+
+    def save(self, commit=True):
+        admin = super().save(commit=False)
+        password = self.cleaned_data.get('password')
+        if admin.id is None or not admin.check_password(password):
+            admin.set_password(password)
+            if commit:
+                admin.save()
+        return admin
