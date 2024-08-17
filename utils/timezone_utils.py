a755123b4cb9fab762fe1bbcff804ffb7209e1e4
공경섭 <kyong.dev@gmail.com>
2024-08-17 19:47:05 +0900

    timezone util

--- utils/timezone_utils.py
+++ utils/timezone_utils.py
@@ -5,3 +5,6 @@ def get_start_of_today():
 
 def get_start_of_date(date):
     return date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+def timezone_convert(datetime_at):
+    return timezone.localtime(datetime_at).strftime('%Y-%m-%d %H:%M:%S')
